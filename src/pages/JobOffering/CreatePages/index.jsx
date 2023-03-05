be5b349e39ea5a06264dfdd859c1ead0371aa977
nghia <nghia14302@gmail.com>
2023-03-05 18:42:48 +0700

    UPDATE - reverse to origin file of branch dev

--- src/pages/JobOffering/CreatePages/index.jsx
+++ src/pages/JobOffering/CreatePages/index.jsx
@@ -38,6 +38,7 @@ const PostCreation = () => {
 		const completedForm = {
 			id: params.id,
 			...formData,
+			createdTime: data.createdTime,
 			description: JSON.stringify(raw),
 		};
 		if (params.id) {
